323ddb03fc63e293c8d4f048ae3b15dbbc8926d6
babaknasrollahy <120930271+babaknasrollahy@users.noreply.github.com>
2023-04-28 11:22:16 +0330

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Start to BUILD Quote_App ........"
-build -t babaknasrolahy/qoute:latest ./QuoteService/app.dockerfile
+docker build -t babaknasrolahy/qoute:latest ./QuoteService/app.dockerfile
 echo "Start to PUSH Quote_App on the DockerHub ..... "
 docker push babaknasrolahy/qoute:latest
 
@@ -8,7 +8,7 @@ echo "-------------------------------------------------------------"
 
 
 echo "Start to BUILD ApiGateway ........"
-build -t babaknasrolahy/api_qoute:latest ./ApiGateway/app.dockerfile
+docker build -t babaknasrolahy/api_qoute:latest ./ApiGateway/app.dockerfile
 echo "Start to PUSH ApiGateway on the DockerHub ..... "
 docker push babaknasrolahy/api_qoute:latest
 
@@ -16,7 +16,7 @@ echo "-------------------------------------------------------------"
 
 
 echo "Start to BUILD FrontEnd_App ........"
-build -t babaknasrolahy/front_qoute:latest ./FrontendApplication/app.dockerfile
+docker build -t babaknasrolahy/front_qoute:latest ./FrontendApplication/app.dockerfile
 echo "Start to PUSH FrontEnd_App on the DockerHub ..... "
 docker push babaknasrolahy/front_qoute:latest
 
